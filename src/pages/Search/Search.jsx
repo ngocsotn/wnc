0d7a66b47803cf5d1cb31a190fa081fbd2ae92c1
ngocsotn <nguyenthengoc1998@live.com>
2021-11-01 20:07:24 +0700

    complete search/category page

--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -1,24 +1,60 @@
-import { Box, Container, Grid } from '@material-ui/core';
+import { Box, Container, Grid, Typography } from '@material-ui/core';
+import Pagination from '@material-ui/lab/Pagination';
 import React from 'react';
-import ProductSlider from '../../components/ProductSlider/ProductSlider';
+import ProductItem from '../../components/ProductItem/ProductItem';
 import SectionTitle from '../../components/SectionTitle/SectionTitle';
 import useStyles from './Search.styles';
 
 function Search() {
-	// const classes = useStyles();
+	const keyword = "";
+	const classes = useStyles();
+
   return (
 		<>
-			<select>
-				<option value={10}>Tăng dần</option>
-				<option value={20}>Giảm dần</option>
-			</select>
-			<select>
-				<option value={10}>Thời gian kết thúc</option>
-				<option value={20}>Giá cả</option>
-			</select>
+			<div className={classes.wrap}></div>
+				<div className={classes.filter_warp}>
+					<div className={classes.filter_item}>
+						<Typography variant="p">Sắp theo:</Typography>
+						<select>
+							<option value={10}>Tăng dần</option>
+							<option value={20}>Giảm dần</option>
+						</select>
+					</div>
+					<div className={classes.filter_item}>
+						<Typography variant="p">Tiêu chí:</Typography>
+						<select>
+							<option value={10}>Thời gian kết thúc</option>
+							<option value={20}>Giá cả</option>
+						</select>
+					</div>
+				</div>
+				<SectionTitle title="Xem sản phẩm theo danh mục/Từ khóa"/>
+				<div className={classes.result}>
+				<Grid container spacing={5}>
+				{[...Array(20)].map((x, i) =>
+					<Grid item xs={4} md={3}>
+						<ProductItem
+							ket={i}
+							productId={i}
+							title="Đồng hồ Rolex"
+							imgSrc="https://cdn2.jomashop.com/media/catalog/product/cache/fde19e4197824625333be074956e7640/r/o/rolex-cosmograph-daytona-chronograph-automatic-rainbow-pave-watch-116595rbow0002.jpg?width=546&height=546"
+							categoryName="Đồng hồ"
+							categoryId={1}
+							dateCreated="01/12/2021 12:00:00"
+							dateEnd="01/11/2021 23:01:00"
+							totalBid={10}
+							currentPrice={12000000}
+							currentUser="xizot"
+							currentUserRate={99}
+						/>
+					</Grid>
+				)}
+				</Grid>
+				<div className={classes.pagination}>
+					<Pagination count={10} variant="outlined" shape="rounded" />
 
-			<SectionTitle title="Gần kết thúc" />
-			<ProductSlider listProduct={[1, 1, 1, 1, 1, 1, 1]} slidesToShow={4} />
+				</div>
+			</div>
     </>
   );
 }
